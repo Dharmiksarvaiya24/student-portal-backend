2de59100f7943fffe9de3c91f2fdfe57b05d3877
Dharmiksarvaiya24 <dharmiksarvaiya07@gmail.com>
2024-01-22 18:17:28 +0530

    function added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+// function added
 
 const app = express();
 app.use(cors());
