54fd25641ea2d7d5789c52e305cf78b317bc250f
Dharmiksarvaiya24 <dharmiksarvaiya07@gmail.com>
2024-02-10 18:44:25 +0530

    update

--- server.js
+++ server.js
@@ -1,62 +1,31 @@
-// edited by roar
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// function 1 added
 
 const app = express();
 app.use(cors());
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/user', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://dharmiksarvaiya07:Dharmik%402426@userdata.cr5gck8.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to Database'))
   .catch(error => console.error(error));
-
 
 app.use(bodyParser.json());
 
 const User = mongoose.model('User', {
     name:{
         type: String,
-        require: true
+        required: true
     },
     email:{
         type: String,
-        require: true
+        required: true
     },
     city:{
         type: String,
-        require: true
+        required: true
     }    
 });
-
-app.post('/', async (req, res) => {
-  try {
-    const { name, email, city } = req.body;
-    const newUser = new User({ 
-      name: String(name),
-      email: String(email),
-      city: String(city),
-    });
-    await newUser.save();
-  } catch (error) {
-    console.error(error);
-        console.log(error);
-  }
-});
-
-app.delete('/:userId', async (req, res) => {
-  const userId = req.params.userId;
-
-  try {
-    const user = await User.findByIdAndDelete(userId);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-
 
 app.get('/', async (req, res) => {
   try {
@@ -67,6 +36,40 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.post('/', async (req, res) => {
+  try {
+    const { name, email, city } = req.body;
+    const newUser = new User({ 
+      name,
+      email,
+      city,
+    });
+    await newUser.save();
+    res.json(newUser);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.delete('/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.json('User deleted successfully');
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get('/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await User.findById(userId);
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.put('/:userId', async (req, res) => {
   const userId = req.params.userId;
@@ -74,14 +77,12 @@ app.put('/:userId', async (req, res) => {
 
   try {
     const user = await User.findByIdAndUpdate(userId, { name, city, email }, { new: true });
-    
+    res.json(user);
   } catch (error) {
     console.error(error);
   }
 });
 
-
-
-app.listen(8080, ()=>{
-    console.log("Server started at port")
-})
+app.listen(8080, () => {
+    console.log("Server started at port 8080");
+});
